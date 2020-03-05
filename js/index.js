ff558087284908c93c1fb5d5ef97321dbc7a452b
graphic <graphic@WFD228-36m.local>
2020-03-05 10:38:34 -0500

    updated interface

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
 const $week = document.querySelector('.week')
 
-const clearAllSelected = () => document.querySelectorAll('.selected').forEach(sel => sel.classList.remove('selected'))
-document.querySelector('.clear').addEventListener('click', clearAllSelected);
-
-
 document.querySelector('.a').addEventListener('click', event => {
 	clearAllSelected()
 	$week.firstElementChild.classList.add('selected')
@@ -24,31 +20,56 @@ document.querySelector('.c').addEventListener('click', event => {
 document.querySelector('.d').addEventListener('click', event => {
 	clearAllSelected()
 	$week.children[3].classList.add('selected')
-});
-
+});        
 
 document.querySelector('.e').addEventListener('click', event => {
 	let $selected = document.querySelector('.selected') // Store the selected element before clearing it!
 	
-	clearAllSelected()
-	$selected.parentElement.classList.add('selected') // Relative to what was selected before, go up the tree!
+	if ($selected != document.body) {
+		clearAllSelected()
+		$selected.parentElement.classList.add('selected') // Relative to what was selected before, go up the tree!
+	}
 });
 
-
 document.querySelector('.f').addEventListener('click', event => {
+	let $selected = document.querySelector('.selected') // Store the selected element before clearing it!
+
+	clearAllSelected()
+	$selected.closest('.calendar').classList.add('selected')
+});
+
+document.querySelector('.g').addEventListener('click', event => {
 	let $selected = document.querySelector('.selected') // Store the selected element before clearing it!
 
 	clearAllSelected()
 	$selected.nextElementSibling.classList.add('selected')
 });
 
-
-document.querySelector('.g').addEventListener('click', event => {
+document.querySelector('.h').addEventListener('click', event => {
 	let $selected = document.querySelector('.selected') // Store the selected element before clearing it!
 
 	clearAllSelected()
 	$selected.previousElementSibling.classList.add('selected')
 });
+
+const clearAllSelected = () => {
+	document.querySelectorAll('.selected').forEach(sel => sel.classList.remove('selected'))
+	document.querySelector('.console').classList.remove('shown')
+}
+const selectTheWeekList = () => { 
+	clearAllSelected()
+	$week.classList.add('selected') 
+}
+window.addEventListener('error', event => {
+	document.querySelector('.console').classList.add('shown')
+	document.querySelector('.console').textContent = `${event.type}: ${event.message}\n`;
+});
+
+// document.querySelector('.getweek').addEventListener('click', selectTheWeekList);
+document.querySelector('.clear').addEventListener('click', clearAllSelected);
+window.addEventListener('load', selectTheWeekList)
+
+
 
 // Share:
 // https://zellwk.com/blog/dom-traversals/
